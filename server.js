fac03291df363002ed0381a1b06757d8141c6ed3
codekcg23 <kavigamage23@gmail.com>
2020-02-26 18:58:33 +0530

    20200226-updaed

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const path = require('path');
 const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV === 'production') {           
 
-
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, "client", "build")));
   
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
